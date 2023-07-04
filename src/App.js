9d42c51b55b7b38fa1b7e1e28a78a939c816c4cd
RAFAEL OLIVA <irvin.rafael.3096@gmail.com>
2023-07-04 10:04:02 -0600

    new files

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import { Navbar, Maps, ScrollToTop, Footer } from './components';
+import { Contact, Services, Header } from './containers';
+
+const App = () => {
+  return (
+    <div className = 'App'>
+      <div className = 'landing__pg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Services />
+      <Contact />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
