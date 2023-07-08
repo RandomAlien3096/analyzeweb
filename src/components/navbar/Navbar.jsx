7d4cc83a3f8a04920628655edf2d699ac0ba420c
RAFAEL OLIVA <irvin.rafael.3096@gmail.com>
2023-07-08 12:56:28 -0600

    navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,19 +1,27 @@
 import React, { useState, useEffect, useRef } from 'react';
 import './navbar.css';
+
 import logo_color from '../../rsrc/logo/logo_color.png';
+import { IoMdMenu, IoMdFingerPrint } from 'react-icons/io';
+import { RiHome6Line } from 'react-icons/ri';
+import { HiOutlineCubeTransparent, HiOutlinePaperAirplane } from 'react-icons/hi';
 
 
 const Menu = () => (
   <>
+    <RiHome6Line />
     <p>
       Inicio
     </p>
+    <IoMdFingerPrint />
     <p>
       Nosotros
     </p>
+    <HiOutlineCubeTransparent />
     <p>
       Servicios
     </p>
+    <HiOutlinePaperAirplane />
     <p>
       Contacto
     </p>
@@ -52,7 +60,7 @@ const Navbar = () => {
       </div>
       <div className = 'analyzeweb__navbar-menu2' ref = {menuRef}>
         <button className='analyzeweb__navbar-menu2_trigger' onClick={() => setOpen(!open)}>
-          <h3>Menu</h3>
+          <h3>Menu</h3><IoMdMenu />
         </button>
         <div className={`analyzeweb__navbar-menu2_dropdown ${open? 'active': 'inactive'}`}>
           <div className='analyzeweb__navbar-menu2_dropdown-links'>
