59e1ad670fcb1b6913723ec1f89c5de605cc231a
RAFAEL OLIVA <irvin.rafael.3096@gmail.com>
2023-07-09 12:00:59 -0600

    header and graphics animated on header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -6,22 +6,26 @@ import graphs from '../../rsrc/imgs/abstract_data.jpg';
 const Header = () => {
   return (
     <div className = 'analyzeweb__header'>
-      <div className = 'analyzeweb__header-slogan'>
+      <div className = 'analyzeweb__header-content'>
         <h2>CONVERSACION</h2>
         <h2>MEDIANTE</h2>
         <h2>INMERSION</h2>
         <h2>DIGITAL</h2>
-      </div>
-      <div className = 'analyzeweb__header-button'>
-        <button className='analyzeweb__header-button_trigger'>
-          <p>Descubre lo que hacemos</p>
-        </button>
+        <div className = 'analyzeweb__header-content_button'>
+          <button id = 'trigger'>
+            <p>Descubre lo que hacemos</p>
+          </button>
+        </div>
       </div>
       <div className = 'analyzeweb__header-graphics'>
-        <div id = 'blob'></div>
-        <div id = 'blur'></div>
+        <div className = 'analyzeweb__header-graphics_blob'>
+          <div id = 'blob'></div>
+          <div id = 'blob'></div>
+          <div id = 'blob'></div>
+        </div> 
         <img src = {graphs} alt='graph deco' id='graphs' />
       </div>
+      
     </div>
   )
 }
