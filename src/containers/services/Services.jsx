9952491869adb96bbf85cec4f269840fbeb9332f
RAFAEL OLIVA <irvin.rafael.3096@gmail.com>
2023-07-09 21:27:19 -0600

    services

--- src/containers/services/Services.jsx
+++ src/containers/services/Services.jsx
@@ -4,7 +4,26 @@ import './services.css';
 const Services = () => {
   return (
     <div className = 'analyzeweb__services'>
-        
+      <div className = 'analyzeweb__services-title'>
+        <h2>TU FUTURO A LA MEDIDA</h2>
+      </div>
+      <div className = 'analyzeweb__services-carousel'>
+        <div id = 'card'>
+          <h4>Modelos Predictivos</h4>
+        </div>
+        <div id = 'card'>
+          <h4>Desarrollo Web</h4>
+        </div>
+        <div id = 'card'>
+          <h4>Optimizacion Financiera BI</h4>
+        </div>
+        <div id = 'card'>
+          <h4>Gobernacion de Data</h4>
+        </div>
+        <div id = 'card'>
+          <h4>Estrategias de Informacion</h4>
+        </div>
+      </div>   
     </div>
   )
 }
