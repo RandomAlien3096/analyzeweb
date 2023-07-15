1c5037382ef6e90b1cf1b7bca43253dad7f127d8
RAFAEL OLIVA <irvin.rafael.3096@gmail.com>
2023-07-15 16:56:47 -0600

    parallax effect not working

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import './app.css';
 import { Navbar, Maps, ScrollToTop, Footer } from './components';
 import { Contact, Services, Header } from './containers';
 
+import { ParallaxProvider } from 'react-scroll-parallax';
+
 const App = () => {
   return (
-    <div className = 'App'>
-      <div className = 'landing__pg'>
-        <Navbar />
-        <Header />
+    <ParallaxProvider scrollAxis = 'horizontal'>
+      <div className = 'App'>
+        <div className = 'landing__pg'>
+          <Navbar />
+          <Header />
+        </div>
+        <Services />
+        <Contact />
+        <Footer />
       </div>
-      <Services />
-      <Contact />
-      <Footer />
-    </div>
+    </ParallaxProvider>
   )
 }
 
--- src/containers/services/Services.jsx
+++ src/containers/services/Services.jsx
@@ -6,9 +6,7 @@ import opt from '../../rsrc/imgs/Optimizacion_bi.jpg';
 import data_gov from '../../rsrc/imgs/data_gov.jpg';
 import data_strat from '../../rsrc/imgs/data_strat.jpg';
 
-import ImageSlider from './ImageSlider';
-
-
+import { Parallax } from 'react-scroll-parallax';
 
 const Services = () => {
   return (
@@ -17,25 +15,28 @@ const Services = () => {
         <h2>TU FUTURO A LA MEDIDA</h2>
       </div>
       <div className = 'analyzeweb__services-carousel'>
+        <div id='card'>
+          <img src = {pred} className='image_card' draggable = 'false' />
+        </div>
+          {/* <img src = {pred} className='image' draggable = 'false' /> */}
+          {/* <h4>Modelos Predictivos</h4> */}
+        
+
         <div id = 'card'>
-          <img src = {pred} className='image' draggable = 'false' />
-          <h4>Modelos Predictivos</h4>
+          <img src = {des_web} className='image_card' draggable = 'false' />
+          {/* <h4>Desarrollo Web</h4> */}
         </div>
         <div id = 'card'>
-          <img src = {des_web} className='image' draggable = 'false' />
-          <h4>Desarrollo Web</h4>
+          <img src = {opt} className='image_card' draggable = 'false' />
+          {/* <h4>Optimizacion Financiera BI</h4> */}
         </div>
         <div id = 'card'>
-          <img src = {opt} className='image' draggable = 'false' />
-          <h4>Optimizacion Financiera BI</h4>
+          <img src = {data_gov} className='image_card' draggable = 'false' />
+          {/* <h4>Gobernacion de Data</h4> */}
         </div>
         <div id = 'card'>
-          <img src = {data_gov} className='image' draggable = 'false' />
-          <h4>Gobernacion de Data</h4>
-        </div>
-        <div id = 'card'>
-          <img src = {data_strat} className='image' draggable = 'false' />
-          <h4>Estrategias de Informacion</h4>
+          <img src = {data_strat} className='image_card' draggable = 'false' />
+          {/* <h4>Estrategias de Informacion</h4> */}
         </div>
       </div>   
     </div>
