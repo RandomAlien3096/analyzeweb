84c4ef25df04327724b78f73ec5a41092a739e82
RAFAEL OLIVA <irvin.rafael.3096@gmail.com>
2023-07-16 15:37:50 -0600

    contact

--- src/containers/contact/Contact.jsx
+++ src/containers/contact/Contact.jsx
@@ -3,8 +3,22 @@ import './contact.css';
 
 const Contact = () => {
   return (
-    <div>
-        
+    <div className = 'analyzeweb__contact'>
+      <div className = 'analyzeweb__contact-content'>
+        <h2>haz de tu proyecto</h2>
+        <h2>nuestra proxima</h2>
+        <h2>obsesion</h2>
+        <div className = 'analyzeweb__contact-content_button'>
+          <button id='trigger'>
+            <span>Hablemos con un cafe</span>
+          </button>
+        </div>
+      </div>
+      <div className = 'analyzeweb__contact-graphics'>
+        <div id='blob'></div>
+        <div id='blob'></div>
+        <div id='blob'></div>
+      </div>
     </div>
   )
 }
