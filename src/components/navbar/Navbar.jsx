1f90088400e746d000edd2bd9881219e1a48417b
RAFAEL OLIVA <irvin.rafael.3096@gmail.com>
2023-07-16 20:53:32 -0600

    addings

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import './navbar.css';
 
 import logo_color from '../../rsrc/logo/logo_color.png';
+import logo_dark from '../../rsrc/logo/logo_dark.png';
 import { IoMdMenu, IoMdFingerPrint } from 'react-icons/io';
 import { RiHome6Line } from 'react-icons/ri';
 import { HiOutlineCubeTransparent, HiOutlinePaperAirplane } from 'react-icons/hi';
@@ -31,9 +32,9 @@ const Menu = () => (
 
 const Navbar = () => {
   const [ open, setOpen ] = useState(false);
-
+  
   let menuRef = useRef();
-
+  
   useEffect(() => {
     let handler = (e) =>{
       if(!menuRef.current.contains(e.target)){
@@ -42,24 +43,56 @@ const Navbar = () => {
       }
     };
     document.addEventListener("mousedown", handler);
-
+    
     return() => {
       document.removeEventListener("mousedown", handler);
     }
   });
+  //change logo on scroll
+  const [logo, setLogo] = useState(false);
+  const changeLogo = () => {
+    console.log(window.scrollY)
+    const vhEquivalent = 2 * window.innerHeight;
+    console.log('vhEquivalent', vhEquivalent)
+    console.log('innerHeight', window.innerHeight)
+    console.log('totalHeight', window.document.body.offsetHeight)
+    if(window.scrollY >= vhEquivalent){
+      setLogo(true)
+    }
+    else {
+      setLogo(false)
+    }
+  };
 
+  window.addEventListener('scroll', changeLogo);
 
   return (
     <div className = 'analyzeweb__navbar'>
-      <div className = 'analyzeweb__navbar-logo'> 
-        <img src = {logo_color} alt='main logo' id='logo' />
-        <p>analyzeweb</p>
+      <div className="analyzeweb__navbar-logo">
+        {logo ? (
+          <>
+            <img src={logo_dark} alt="main logo" 
+            id="logo_dark"
+            className={logo ? 'show' : ''} />
+            <p>analyzeweb</p>
+          </>
+        ) : (
+          <>
+            <img src={logo_color} alt="main logo" 
+            id="logo" 
+            className={logo ? 'show' : ''}/>
+            <p>analyzeweb</p>
+          </>
+        )}
       </div>
       <div className = 'analyzeweb__navbar-links'>
         <Menu />
       </div>
       <div className = 'analyzeweb__navbar-menu2' ref = {menuRef}>
-        <button className='analyzeweb__navbar-menu2_trigger' onClick={() => setOpen(!open)}>
+        <button className={logo ? 
+          'analyzeweb__navbar-menu2_trigger icon_color' 
+          : 'analyzeweb__navbar-menu2_trigger'} 
+          onClick={() => setOpen(!open)}>
           <h3>Menu</h3><IoMdMenu />
         </button>
         <div className={`analyzeweb__navbar-menu2_dropdown ${open? 'active': 'inactive'}`}>
