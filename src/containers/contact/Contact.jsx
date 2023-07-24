4081350fc79d91a2bde444f366ada3134d6bc317
RAFAEL OLIVA <irvin.rafael.3096@gmail.com>
2023-07-23 22:49:02 -0600

    start of contactPg

--- src/containers/contact/Contact.jsx
+++ src/containers/contact/Contact.jsx
@@ -9,13 +9,11 @@ const Contact = () => {
         <h2>haz de tu proyecto</h2>
         <h2>nuestra proxima</h2>
         <h2>obsesion</h2>
-        <div className = 'analyzeweb__contact-content_button'>
-          <Link to = '/ContactPg' className='analyzeweb__contact-content_button'>
-            <button type='button' id='trigger'>
-              <span>Hablemos con un cafe</span>
-            </button>
-          </Link>
-        </div>
+        <Link to = '/ContactPg' className='analyzeweb__contact-content_button'>
+          <button type='button' id='trigger'>
+            <span>Hablemos con un cafe</span>
+          </button>
+        </Link>
       </div>
       <div className = 'analyzeweb__contact-graphics'>
         <div id='blob'></div>
--- src/pages/contactPg/ContactPg.jsx
+++ src/pages/contactPg/ContactPg.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import './contactPg.css';
+import { Footer, Navbar } from '../../components';
 import emailjs from '@emailjs/browser';
 
 function DatagovPg() {
     const serviceID = process.env.REACT_APP_EMAILJS_SERVICE_ID;
 
     return (
-        <div>
-            <h1>{serviceID}</h1>
+        <div className = "analyzeweb__contactPg">
+            <div className = "analyzeweb__contactPg-navbar">
+                <Navbar />
+            </div>
+            <div className = "analyzeweb__contactPg-footer">
+                <Footer />
+            </div>
         </div>
     )
   }
