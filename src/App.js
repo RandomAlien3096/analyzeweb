cfa97a8f8ebba604681bd89014ea7745516565d9
RAFAEL OLIVA <irvin.rafael.3096@gmail.com>
2023-07-29 18:46:38 -0600

    general fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import './app.css';
@@ -10,28 +10,49 @@ import { ContactPg, DatagovPg, DatastratPg,
 import { ParallaxProvider } from 'react-scroll-parallax';
 
 const App = () => {
+  // State to track if JavaScript is enabled or not
+  const [isJavaScriptEnabled, setIsJavaScriptEnabled] = useState(true);
+
+  // useEffect hook to check if JavaScript is enabled on mount
+  useEffect(() => {
+    setIsJavaScriptEnabled(true);
+  }, []);
+
   return (
     <ParallaxProvider>
-      <Routes>
-        <Route path = '/' element={
-          <div className = 'App'>
-            <div className = 'landing__pg'>
-              <Navbar />
-              <Header />
-            </div>
-            <Services />
-            <Contact />
-            <Footer />
-          </div> } />
-        <Route path = '/ContactPg' element = { <ContactPg /> } />
-        <Route path = '/DatagovPg' element = { <DatagovPg /> } />
-        <Route path = '/DatastratPg' element = { <DatastratPg /> } />
-        <Route path = '/DesarrolloPg' element = { <DesarrolloPg /> } />
-        <Route path = '/ModelosPg' element = { <ModelosPg /> } />
-        <Route path = '/OptimizacionPg' element = { <OptimizacionPg /> } />
-      </Routes>
+      {isJavaScriptEnabled ? (
+        // Render the app content when JavaScript is enabled
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div className="App">
+                <div className="landing__pg">
+                  <Navbar />
+                  <Header />
+                </div>
+                <Services />
+                <Contact />
+                <Footer />
+              </div>
+            }
+          />
+          <Route path="/ContactPg" element={<ContactPg />} />
+          <Route path="/DatagovPg" element={<DatagovPg />} />
+          <Route path="/DatastratPg" element={<DatastratPg />} />
+          <Route path="/DesarrolloPg" element={<DesarrolloPg />} />
+          <Route path="/ModelosPg" element={<ModelosPg />} />
+          <Route path="/OptimizacionPg" element={<OptimizacionPg />} />
+        </Routes>
+      ) : (
+        // Fallback content for when JavaScript is disabled
+        <div>
+          <h1>You need to enable JavaScript to run this app.</h1>
+          {/* You can provide more specific instructions or alternative content here */}
+        </div>
+      )}
     </ParallaxProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
