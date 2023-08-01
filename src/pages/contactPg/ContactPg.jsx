232db9fbf60ceb96ee71532e7dc5f3eac2b88086
RAFAEL OLIVA <irvin.rafael.3096@gmail.com>
2023-07-31 22:41:24 -0600

    contactPg form

--- src/pages/contactPg/ContactPg.jsx
+++ src/pages/contactPg/ContactPg.jsx
@@ -28,9 +28,11 @@ function DatagovPg() {
 
     return (
         <div className = "analyzeweb__contactPg">
+
             <div className = "analyzeweb__contactPg-navbar">
                 <Navbar />
             </div>
+
             <div className = "analyzeweb__contactPg-content">
                 <div className = "analyzeweb__contactPg-content_title">
                     <h1>Hablemos con un cafe.</h1>
@@ -42,27 +44,55 @@ function DatagovPg() {
                 <div className = "analyzeweb__contactPg-form_title">
                     <h2>Dinos, como podemos ayudar?</h2>
                 </div>
+
+
                 <form 
                     className = "analyzeweb__contactPg-form_form" 
                     ref={form} 
-                    onSubmit={sendEmail}>
-                    <div id = "name_title">
-                        <h5>Nombre Completo</h5>
+                    onSubmit={sendEmail} >
+                    
+                    <div className="analyzeweb__contactPg-form_form-name">
+                        <div id = "name_title">
+                            <h5>Nombre Completo</h5>
+                        </div>
+                        <input 
+                            id = "name_input" 
+                            type = 'text' 
+                            name = 'name_input' required 
+                        />
                     </div>
-                    <input 
-                        id = "name_input" 
-                        type = 'text' 
-                        name = 'name_input' required 
-                    />
-                    <div id = 'email_title'>
-                        <h5>Email</h5>
+
+                    <div className="analyzeweb__contactPg-form_form-email">
+                        <div id = 'email_title'>
+                            <h5>Email</h5>
+                        </div>
+                        <input 
+                            id = 'email_input'
+                            type = 'email'
+                            name = 'email_input' required 
+                        />
                     </div>
-                    <input 
-                        id = 'email_input'
-                        type = 'email'
-                        name = 'email_input' required 
-                    />
+
+                    <div className="analyzeweb__contactPg-form_form-text">
+                        <div id = 'text_title'>
+                            <h5>Cuentanos de tu Proyecto</h5>
+                        </div>
+                        <textarea
+                            id = 'text_input'
+                            name = 'text_input' 
+                        />
+                    </div>
+
+                    <div className="analyzeweb__contactPg-form_form-button">
+                        <button 
+                            type = 'submit'
+                            value = 'send' >
+                            Mandar Mensaje
+                        </button>
+                    </div>
                 </form>
+
+
             </div>
             <div className = "analyzeweb__contactPg-footer">
                 <Footer />
