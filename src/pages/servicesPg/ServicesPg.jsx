38531450b6386725cd4bb1b6a3cbdff381d81565
RAFAEL OLIVA <irvin.rafael.3096@gmail.com>
2023-08-03 01:50:45 -0600

    servicesPg

--- src/pages/servicesPg/ServicesPg.jsx
+++ src/pages/servicesPg/ServicesPg.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './servicesPg.css';
+import { Navbar, Footer } from '../../components';
 
 function ServicesPg() {
   return (
-    <>
-        <div>ServicesPg</div>
+    <> 
+        <div className = 'analyzeweb__servicesPg-navbar'>
+            <Navbar />
+        </div>
+        <div className = 'analyzeweb__servicesPg'>
+            <h1>ServicesPg</h1>
+        </div>
+        <div className = 'analyzeweb__services-footer'>
+            <Footer />
+        </div>
     </>
   )
 }
