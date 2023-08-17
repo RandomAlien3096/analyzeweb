f77637e3edb98b2bedf85813d3f489a02ae8a815
RAFAEL OLIVA <irvin.rafael.3096@gmail.com>
2023-08-17 03:16:08 -0600

    changes in style of desarrolloPg

--- src/pages/desarrolloPg/DesarrolloPg.jsx
+++ src/pages/desarrolloPg/DesarrolloPg.jsx
@@ -6,11 +6,32 @@ import { AiOutlineCheck } from "react-icons/ai";
 
 function DesarrolloPg() {
 
-  const listItems = [
+  const listBasico = [
     '1 pagina web landing',
     'Dominio web',
     '2 correos electronicos',
     'Posicionamiento SEO',
+  ];
+
+  const listCompleto = [
+    '2 Paginas web landing',
+    '3 Secciones por pagina',
+    'Dominio web',
+    '5 correos electronicos',
+    'Posicionamiento SEO',
+    'Analisis Estadistico Interno',
+  ];
+
+  const listPremium = [
+    '3 Paginas web landing',
+    '3 Secciones por pagina',
+    'Dominio web',
+    '5 correos electronicos/pagina',
+    'Posicionamiento SEO',
+    'Analisis estadistico interno',
+    'Analisis perfiles clientes',
+    'Tendencia mercado interna',
+    'Tecnologias BI aplicadas',
   ];
 
   return (
@@ -24,11 +45,13 @@ function DesarrolloPg() {
           <h3>Empieza tu huella en linea con nuestros planes</h3>
         </div>
         <div className = 'analyzeweb__desarrolloPg-content'>
+
           <div id = 'card'>
             <div id = 'card-top'>
               <h4>Basico</h4>
               <h5>1 Pagina Web</h5>
-              <h3>$199.00</h3>
+              <h3>$ 199</h3>
+              <p>Pago unico *</p>
               <button>
                 <span>Inicia Hoy</span>
               </button>
@@ -36,18 +59,66 @@ function DesarrolloPg() {
             <hr></hr>
             <div id='card-description'>
               <h5>Plan incluye</h5>
-
               <ul style={{ listStyleType: 'none', padding: 0 }}>
-                {listItems.map((item, index) => (
+                {listBasico.map((item, index) => (
                   <li key={index} style={{ marginBottom: '10px', display: 'flex', alignItems: 'center' }}>
                     <AiOutlineCheck style={{ marginRight: '5px' }} />
                     {item}
                   </li>
                 ))}
               </ul>
-
             </div>
           </div>
+
+          <div id = 'card'>
+            <div id = 'card-top'>
+              <h4>Completo</h4>
+              <h5>2 Paginas Web</h5>
+              <h3>$ 299</h3>
+              <p>Pago unico *</p>
+              <button>
+                <span>Inicia Hoy</span>
+              </button>
+            </div>
+            <hr></hr>
+            <div id='card-description'>
+              <h5>Plan incluye</h5>
+              <ul style={{ listStyleType: 'none', padding: 0 }}>
+                {listCompleto.map((item, index) => (
+                  <li key={index} style={{ marginBottom: '10px', display: 'flex', alignItems: 'center' }}>
+                    <AiOutlineCheck style={{ marginRight: '5px' }} />
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+
+          <div id = 'card'>
+            <div id = 'card-top'>
+              <h4>Premium</h4>
+              <h5>3 Paginas Web</h5>
+              <h3>$ 499</h3>
+              <p>Pago unico *</p>
+              <button>
+                <span>Inicia Hoy</span>
+              </button>
+            </div>
+            <hr></hr>
+            <div id='card-description'>
+              <h5>Plan incluye</h5>
+              <ul style={{ listStyleType: 'none', padding: 0 }}>
+                {listPremium.map((item, index) => (
+                  <li key={index} style={{ marginBottom: '10px', display: 'flex', alignItems: 'center' }}>
+                    <AiOutlineCheck style={{ marginRight: '5px' }} />
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+
+
         </div>
       </div>
       <div className = 'analyzeweb__desarrolloPg-footer'>
