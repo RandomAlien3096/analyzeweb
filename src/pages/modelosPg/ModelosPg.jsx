90aa3d96282a831e6e5c142d2e64d804103d941f
RAFAEL OLIVA <irvin.rafael.3096@gmail.com>
2023-08-18 20:02:23 -0600

    modelosPg style and config

--- src/pages/modelosPg/ModelosPg.jsx
+++ src/pages/modelosPg/ModelosPg.jsx
@@ -1,9 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import './modelosPg.css';
+import { Navbar, Footer } from '../../components';
+import { AiOutlineCheck } from "react-icons/ai";
+import { HashLink as Link } from 'react-router-hash-link';
+
+const SECOND = 1000;
+const MINUTE = SECOND * 60;
+const HOUR = MINUTE * 60;
+const DAY = HOUR * 24;
 
 function ModelosPg() {
+
+  const deadline = new Date('September, 30, 2023').toString();
+
+  const parsedDeadline = useMemo(() => Date.parse(deadline), [deadline]);
+  const [time, setTime] = useState(parsedDeadline - Date.now());
+
+  useEffect(() => {
+      const interval = setInterval(
+          () => setTime(parsedDeadline - Date.now()),
+          1000,
+      );
+
+      return () => clearInterval(interval);
+  }, []);
+
+
   return (
-    <div>ModelosPg</div>
+    <>
+      <div className = 'analyzeweb__modelosPg-navbar'>
+        <Navbar />
+      </div>
+
+      <div className='RO__ComingSoonP1'>
+        <div className='RO__ComingSoon-content'>
+          <p>Pagina esta bajo mantenimiento</p>
+          <h1>Lanzando pronto este repertorio</h1>
+        </div>
+        <div className='RO__ComingSoon-timer'>
+              {Object.entries({
+                  Dias: time / DAY,
+                  Horas: (time / HOUR) % 24,
+                  Minutos: (time / MINUTE) % 60,
+                  Segundos: (time / SECOND) % 60,
+              }).map(([label, value]) => (
+                  <div key={label} className="col-4">
+                      <div className="RO__ComingSoon-timer_box">
+                          <p>{`${Math.floor(value)}`.padStart(2, "0")}</p>
+                          <span className="text">{label}</span>
+                      </div>
+                  </div>
+              ))}
+        </div>
+        <div className='RO__ComingSoon-button'>
+          <Link to='/'>
+            <button type='button'>Regreso a Pagina Inicio</button>
+          </Link>
+        </div>
+      </div>
+
+      <div className = 'analyzeweb__modelosPg-footer'>
+        <Footer />
+      </div>
+    </>
+
   )
 }
 
--- src/pages/optimizacionPg/OptimizacionPg.jsx
+++ src/pages/optimizacionPg/OptimizacionPg.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './optimizacionPg.css';
+import { Navbar, Footer } from '../../components';
+import { AiOutlineCheck } from "react-icons/ai";
+import { HashLink as Link } from 'react-router-hash-link';
 
 function OptimizacionPg() {
   return (
